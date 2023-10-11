9ba5aa106a7b8d3a7b2f879e85ff3b1d2b90bb1d
Ramón Gómez <A01284731@tec.mx>
2023-10-10 20:27:21 -0600

    correcciones

--- yConnect/yConnect/FavsPage.swift
+++ yConnect/yConnect/FavsPage.swift
@@ -18,10 +18,7 @@ struct FavsPage: View {
             if organizationModel.organizations.count > 0 {
                 ScrollView{
                     ForEach(organizationModel.organizations, id: \.id) { org in
-                        OrgInfoView(
-                            Org_Name: org.Name,
-                            Org_Des: org.Description,
-                            Org_Tags: org.Tags)
+                        OrgInfoView(Org_Name: org.Name, Org_Des: org.Description, Org_Calf: 0.0)
                         .padding(.bottom, 3)
                     }
                 }
